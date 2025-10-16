3b44d822ef5c36ddfa75a0bfdf29eb30c36a30cd
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-16 20:28:01 +0400

    Update server configuration for public URL and keys

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://mobiletech-backend.onrender.com'),
   app: {
-    keys: env.array('APP_KEYS', ['aSuperSecretKey123', 'anotherSecretKey456']),
+    keys: env.array('APP_KEYS', ['yourSecretKey1', 'yourSecretKey2']),
   },
 });
