3f7872c8e3cb372e6d04a3228d95c43b4a014147
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-20 11:37:46 +0400

    Refactor database configuration to use dynamic client

--- config/database.js
+++ config/database.js
@@ -1,16 +1,29 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) ? {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)
+        } : false,
+      },
+      pool: { min: 2, max: 10 }
+    }
+  };
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false) ? {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)
-      } : false,
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-    debug: false,
-  },
-});
+  };
+};
