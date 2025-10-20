2f377dfcf55443217dc0925738581c04e56089ab
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-20 14:34:57 +0400

    Change PUBLIC_URL to STRAPI_ADMIN_BACKEND_URL

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
-  url: env('PUBLIC_URL', 'https://mobiletech-backend.onrender.com'),
+  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://strapi-backend-bchh.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
