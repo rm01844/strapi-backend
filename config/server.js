9e1461d4a46177ca08a250c65264e69765f5a347
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-21 17:20:04 +0400

    Update server configuration to use environment variables

--- config/server.js
+++ config/server.js
@@ -1,8 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
-  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://strapi-backend-bchh.onrender.com'),
+  url: env('STRAPI_ADMIN_BACKEND_URL'),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS', 'gy1k3v0dhs8s9qf4,zx7c5r3m1t9n8b2q,vp4j2d6l0k5f3a9s,hc8w1e7u2o6t9l4b'),
+    keys: env.array('APP_KEYS'),
   },
 });
