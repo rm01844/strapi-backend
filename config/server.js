69ffdc1efd0c4625d453cc2185689f6edc57bc7a
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-21 17:07:51 +0400

    Update APP_KEYS in server configuration

--- config/server.js
+++ config/server.js
@@ -3,6 +3,6 @@ export default ({ env }) => ({
   port: env.int('PORT', 10000),
   url: env('STRAPI_ADMIN_BACKEND_URL', 'https://strapi-backend-bchh.onrender.com'),
   app: {
-    keys: env.array('APP_KEYS', 'aSuperSecretKey123,anotherSecretKey456'),
+    keys: env.array('APP_KEYS', 'gy1k3v0dhs8s9qf4,zx7c5r3m1t9n8b2q,vp4j2d6l0k5f3a9s,hc8w1e7u2o6t9l4b'),
   },
 });
