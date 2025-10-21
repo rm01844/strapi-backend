bf6dfd767ca686a257eaf72c6067897f39fe5a60
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-21 11:15:18 +0400

    Change database client from SQLite to Postgres

--- config/database.js
+++ config/database.js
@@ -23,9 +23,14 @@ module.exports = ({ env }) => {
   // Fallback for local
   return {
     connection: {
-      client: 'sqlite',
+      client: 'postgres',
       connection: {
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: false,
       },
       useNullAsDefault: true,
     },
