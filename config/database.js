68e86857f398f9b24f1bdac095d7575fbdbc66a3
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-23 10:16:55 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,38 +1,9 @@
-const { parse } = require('pg-connection-string');
-
-module.exports = ({ env }) => {
-  const databaseUrl = env('DATABASE_URL');
-  if (databaseUrl) {
-    const config = parse(databaseUrl);
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: { rejectUnauthorized: false },
-        },
-        pool: { min: 0, max: 10 },
-      },
-    };
-  }
-
-  // Fallback for local
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false,
-      },
-      useNullAsDefault: true,
+      filename: env('DATABASE_FILENAME', './data.db'),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
