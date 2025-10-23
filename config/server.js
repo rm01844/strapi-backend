cf71e124d1ca664b811e5e94da4dce480e6e6369
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-23 10:18:51 +0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
-  url: env('STRAPI_ADMIN_BACKEND_URL'),
+  url: env('STRAPI_ADMIN_BACKEND_URL', 'https://strapi-backend-bchh.onrender.com'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
