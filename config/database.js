fc0839b059c92847ce1fbcfe75cc81a95b30f543
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-23 11:10:03 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: env('DATABASE_FILENAME', './data.db'),
+      filename: env('DATABASE_FILENAME', 'data.db'),
     },
     useNullAsDefault: true,
   },
