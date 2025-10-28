0a4a347b5ea76a4ce85ddd75e6d3fdd4d9976ed9
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-28 17:20:08 +0400

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,33 @@
 module.exports = [
   'strapi::errors',
   {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://market-assets.strapi.io',
+          ],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['*'],  // or specify your frontend URL
+      origin: ['*'],
+      headers: '*',
     },
   },
-  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
