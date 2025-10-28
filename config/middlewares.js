268f0410991168157486f5f25fd26eb7f3da6f92
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-28 17:14:04 +0400

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,28 +1,13 @@
 module.exports = [
   'strapi::errors',
   {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'img-src': ["'self'", 'data:', 'blob:'],
-          'media-src': ["'self'", 'data:', 'blob:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['*'], // or ['https://your-frontend-domain']
-      headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      origin: ['*'],  // or specify your frontend URL
     },
   },
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
