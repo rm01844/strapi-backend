45f63e91590dc023f8b6a52766937033faf7e949
Raqeeb Mohamad Rafeek <127798145+rm01844@users.noreply.github.com>
2025-10-28 19:14:41 +0400

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'aqoUtL6PNO98RLqi3mM5Mw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'CTzR2Q72NOMdKMKO/WxjZw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'LfBa1NLgMyRVsFOt+K00tw=='),
     },
   },
   flags: {
